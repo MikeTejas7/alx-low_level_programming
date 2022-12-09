bd3b18fdbc1e9ce0eb725eebaba693070f48127c
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-08 22:40:29 -0800

    Task 4r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- * *main - Put out a string
- *
- * *
- *
- * * Return: Always 0 (success)
- *
- * */
+ *  main - Put out a string
+ *  Return: Always 0 (success)
+ */
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
