10db67031c9ac50a4f765ec43fae3b81464cfc71
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-08 21:08:30 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * *main - Put out a string
+ *
+ * *
+ *
+ * * Return: Always 0 (success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
