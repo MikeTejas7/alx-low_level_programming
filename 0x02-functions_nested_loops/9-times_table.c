c8011deed7bfb584babdab1624b56b1852dbc2f9
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-14 01:51:20 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,40 @@
  */
 void times_table(void)
 {
-	int x, y, z;
+	int x = 0; /* factor */
+	int y; /* count */
+	int z; /* computed value */
 
-	for (x = 0; x <= 9; x++)
+	while (x < 10)
 	{
-		_putchar('0');
-	for (y = 1; y <= 9; y++)
-	{
-		_putchar(',');
-		_putchar(' ');
-		z = x * y;
-		if (z <= 9)
-			_putchar(' ')
-		else
-		_putchar((z / 10) + '0');
-		_putchar((z % 10) + '0');
-	}
-	_putchar('\n');
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
 	}
 }
