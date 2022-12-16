6f71257832b8f04de1a73914818a2cfb70f1df96
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-16 04:01:53 -0800

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,7 +19,7 @@ void print_triangle(int size)
 		{
 			for (spaces = size - row; spaces >= 1; spaces--)
 			{
-				_putchar(' ')
+				_putchar(' ');
 			}
 			for (hashes = 1; hashes <= row; hashes++)
 			{
