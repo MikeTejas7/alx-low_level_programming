ddd15e8cef80ce5e831003b546eef1460d52c0ac
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-16 01:23:25 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Fx that checks for uppercase xter
+ * @c: int c
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 91)
+		return (1);
+	else
+		return (0);
+}
