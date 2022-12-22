90cd9857f60296048b88bf0d181bd07e8607b01c
MikeTejas7 <ndubuisichime6@gmail.com>
2022-12-22 00:27:48 -0800

    Fx that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - Fx that copies a string
+ * @dest: 1st string
+ * @src: 2nd string
+ * @n: int n
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
